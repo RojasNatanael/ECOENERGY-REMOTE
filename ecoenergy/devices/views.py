d669aa9d805807c35418d4f97bb942b24d5c6a5d
officialSES <official.ses@protonmail.com>
2025-10-26 20:51:23 -0300

    Session settings & view

--- ecoenergy/devices/views.py
+++ ecoenergy/devices/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+
+def dashboard(request):
+	visitas = request.session.get('visitas', 0)
+	request.session['visitas'] = visitas + 1
+	return render(request, 'dashboard.html', {'visitas':visitas})
